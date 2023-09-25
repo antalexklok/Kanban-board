623e8486c7602670cdd7c1ac2140994bc8307c8f
antalexklok <119968579+antalexklok@users.noreply.github.com>
2023-09-25 21:37:58 +0300

    Add files via upload

---
+++ src/Components/Footer/footer.tsx
@@ -0,0 +1,18 @@
+import style from "./footer.module.css";
+import { useGlobalContext } from "../Context/taskContext";
+
+export const Footer = () => {
+  const { getActiveTaskCount, getFinishedTaskCount } = useGlobalContext();
+
+  return (
+    <footer className={style.footer}>
+      <div className={style.areaInfo}>
+        <div className={style.tasksCount}>
+          <div>Active tasks: {getActiveTaskCount()}</div>
+          <div>Finished tasks: {getFinishedTaskCount()}</div>
+        </div>
+        <div>Kanban board, 2023</div>
+      </div>
+    </footer>
+  );
+};
